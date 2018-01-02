7487b9e14aa5ae402798f1e4a0b85de7a0820b32
Jun Du <junqdu@gmail.com>
2018-01-02 00:28:54 -0800

    add style

--- pages/want-to-play.vue
+++ pages/want-to-play.vue
@@ -225,5 +225,10 @@ export default {
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 }
+
+input {
+  margin-right: 0.5rem;
+  width: 8.5rem
+}
 </style>
 <style src="./table.less" lang="less" scoped></style>
