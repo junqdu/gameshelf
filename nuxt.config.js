8747ded5db51412d86f4e8da456c6ff3af72a159
Jun Du <junqdu@gmail.com>
2018-01-12 11:23:15 -0800

    update site description

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'A simple and elegant presentation of your board game collection.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
